56483622ead4e5e7f147f57fbe1c1ddd483819a8
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-02-10 20:54:16 -0500

    auth users

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-// const googleAIRoutes = require('./routes/GeminiAPI');
+const mongoose = require('mongoose');
 const visionAIRoute = require('./routes/CloudWrapper');
-// const openAIWrapper = require('./routes/OpenAIWrapper');
+const authRoutes = require('./routes/AuthUser');
 
 const app = express();
 const PORT = 3007;
 
 // Middleware to handle CORS and JSON body parsing
 app.use(express.static('public'));
-// console.log(path.join(__dirname, '/static'));
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Successfully connected to MongoDB'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+
 // Mounting the routes
-// app.use('/google/generateContent', googleAIRoutes);
 app.use('/vision', visionAIRoute);
-// app.use('/OpenAI', openAIWrapper);
+app.use('/api/auth', authRoutes);
 
 // Starting the server
 app.listen(PORT, () => {
